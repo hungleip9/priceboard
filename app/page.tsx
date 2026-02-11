b07df9a1980732ff2b42b5769782164c82ddae38
hunglq <hunglq.dev@gmail.com>
2026-02-11 17:17:59 +0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
 "use client";
-import { useBinanceSocket } from "@/hooks/useBinanceSocket";
-
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  reset,
+} from "@/store/counterSlice";
+import { RootState } from "@/store";
+import { useSelector, useDispatch } from "react-redux";
 export default function Home() {
-  const { price } = useBinanceSocket({ symbol: "btcusdt" });
-
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <>
-      <div key={price} className="text-red">
-        {price}
-      </div>
-    </>
+    <div>
+      <h1>Counter: {count}</h1>
+      <button className="block border" onClick={() => dispatch(increment())}>
+        Tăng
+      </button>
+      <button className="block border" onClick={() => dispatch(decrement())}>
+        Giảm
+      </button>
+      <button
+        className="block border"
+        onClick={() => dispatch(incrementByAmount(5))}
+      >
+        IncrementByAmount
+      </button>
+      <button className="block border" onClick={() => dispatch(reset())}>
+        Reset
+      </button>
+    </div>
   );
 }
